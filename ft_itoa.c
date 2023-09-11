23fca3e3c7599aab170be93cb3d39eea40a8007f
Nephtys <lucie.camerlynck@hotmail.com>
2023-09-11 14:29:39 +0200

    Normed ft_itoa & ft_split. ft_itoa is fully working and tested

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,30 @@
 /*   By: lcamerly <lcamerly@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:55:09 by lcamerly          #+#    #+#             */
-/*   Updated: 2023/09/11 13:55:09 by lcamerly         ###   ########.fr       */
+/*   Updated: 2023/09/11 14:23:53 by lcamerly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static char	*ft_case_zero(char *dest)
+static	char	*ft_case_zero(char *dest)
 {
-	dest = malloc(sizeof(char) * 2);
 	dest[0] = '0';
 	dest[1] = '\0';
+	return (dest);
+}
+
+static	char	*tab_writing(int nb, unsigned int n, int index, char *dest)
+{
+	if (nb < 0)
+		dest[0] = '-';
+	dest[index--] = '\0';
+	while (n > 9)
+	{
+		dest[index--] = n % 10 + '0';
+		n = n / 10;
+	}
+	dest[index] = n % 10 + '0';
 	return (dest);
 }
 
@@ -29,7 +42,7 @@ char	*ft_itoa(int nb)
 
 	i = 1;
 	if (nb == 0)
-		return (ft_case_zero(dest));
+		return (ft_case_zero(dest = malloc(sizeof(char) * 2)));
 	if (nb < 0)
 	{
 		i++;
@@ -44,24 +57,13 @@ char	*ft_itoa(int nb)
 		i++;
 	}
 	dest = malloc(sizeof(char) * (i + 1));
-	if (nb < 0)
-		dest[0] = '-';
-	dest[i--] = '\0';
-	while (n > 9)
-	{
-		dest[i--] = n % 10 + '0';
-		n = n / 10;
-	}
-	dest[i] = n % 10 + '0';
-	return (dest);
+	return (tab_writing(nb, n, i, dest));
 }
 
 int	main(void)
 {
-	// Test with error cases
-	printf("%s\n", ft_itoa(2147483647));
-	printf("%s\n", ft_itoa(-2147483648));
-	printf("%s\n", ft_itoa(0));
-	printf("%s\n", ft_itoa(1));
-	printf("%s\n", ft_itoa(-1));
+	#include <stdio.h>
+	for (int i = 0; i < 14567890; i + i + 1)
+		printf("%s\n", ft_itoa(i));
+
 }
--- ft_split.c
+++ ft_split.c
@@ -6,50 +6,53 @@
 /*   By: lcamerly <lcamerly@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:55:39 by lcamerly          #+#    #+#             */
-/*   Updated: 2023/09/11 13:55:39 by lcamerly         ###   ########.fr       */
+/*   Updated: 2023/09/11 14:05:31 by lcamerly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int ft_is_sep(char c, char charset)
+int	ft_is_sep(char c, char charset)
 {
 	return (c == charset);
 }
 
-int ft_len_word(char *str, char charset)
+int	ft_len_word(char *str, char charset)
 {
-	int len = 0;
+	int	len;
+
+	len = 0;
 	while ((str[len]) && (!(ft_is_sep(str[len], charset))))
 		len++;
 	return (len + 1);
 }
 
-int ft_len_tab(char *str, char charset)
+int	ft_len_tab(char *str, char charset)
 {
+	int	x;
+	int	words;
 
-	int x = 0;
-	int words = 0;
-
+	x = 0;
+	words = 0;
 	while (str[x])
 	{
-		while(str[x] && ft_is_sep(str[x], charset))
+		while (str[x] && ft_is_sep(str[x], charset))
 			x++;
 		if (str[x])
 			words++;
-		while(str[x] && (!(ft_is_sep(str[x], charset))))
+		while (str[x] && (!(ft_is_sep(str[x], charset))))
 			x++;
 	}
-	return words;
+	return (words);
 }
 
-void ft_write_word(char **tab, char *str, char charset)
+void	ft_write_word(char **tab, char *str, char charset)
 {
+	int	len_tab;
+
 	int x, y, z;
 	x = 0, y = 0;
-	int len_tab;
-
 	len_tab = ft_len_tab(str, charset);
 	while (y < len_tab)
 	{
@@ -63,27 +66,29 @@ void ft_write_word(char **tab, char *str, char charset)
 		while ((str[x]) && (!(ft_is_sep(str[x], charset))))
 			tab[y][z++] = str[x++];
 		tab[y][z] = '\0';
-		y++;	
+		y++;
 	}
 	tab[y] = 0;
 }
 
-char **ft_split(char *str, char charset)
+char	**ft_split(char *str, char charset)
 {
-    char **tab;
-    tab = malloc(sizeof(char *) * (ft_len_tab(str, charset) + 1));
-    if (!(tab))
-    	return 0; 
-    ft_write_word(tab, str, charset);
-    return (tab);
+	char	**tab;
+
+	tab = malloc(sizeof(char *) * (ft_len_tab(str, charset) + 1));
+	if (!(tab))
+		return (0);
+	ft_write_word(tab, str, charset);
+	return (tab);
 }
 
-int		main(void)
-{
-	char **tab;
-	tab = ft_split("j'aime les pates sucrées au sucres", ' ');
-	int	c = 0;
-	while (tab[c])
-		printf("%s\n", tab[c++]);
-}
+// int	main(void)
+// {
+// 	char	**tab;
+// 	int		c;
 
+// 	tab = ft_split("j'aime les pates sucrées au sucres", ' ');
+// 	c = 0;
+// 	while (tab[c])
+// 		printf("%s\n", tab[c++]);
+// }
