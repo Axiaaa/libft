6bcbc08266212b4067473d251060b2dd2412d4fd
Nephtys <lucie.camerlynck@hotmail.com>
2023-09-14 11:22:55 +0200

    Added ft_memcpy.c + updated libft.h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,30 @@
 /*   By: lcamerly <lcamerly@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:33:33 by lcamerly          #+#    #+#             */
-/*   Updated: 2023/09/13 11:40:26 by lcamerly         ###   ########.fr       */
+/*   Updated: 2023/09/14 11:22:40 by lcamerly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memccpy(void *dest, const void *src, size_t n)
+{
+    size_t i;
+    unsigned char *ptr_src;
+    unsigned char *ptr_dest;
+    
+
+    dest = malloc(n);
+    ptr_dest = dest;
+    ptr_src = src;
+    i = 0;
+
+    while (i < n)
+    {
+        *ptr_dest = *ptr_src;
+        i++;
+        ptr_dest++;
+        ptr_src++;
+    }
+    return (dest);
+}
